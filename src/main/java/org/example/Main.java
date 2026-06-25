9a136a15425485323c63dfd3193c7dde235aac82
timekeeper005 <99313318+timekeeper005@users.noreply.github.com>
2026-06-25 17:30:35 +0800

    just a pull

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("Go Bears!");
         System.out.println("abc conjecture!");
+        System.out.println("collatz conjecture");
     }
 }
