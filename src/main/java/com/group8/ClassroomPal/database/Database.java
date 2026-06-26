d21f838de6e37897d143deb31b250dccb7190a91
timekeeper005 <99313318+timekeeper005@users.noreply.github.com>
2026-06-26 10:06:12 +0800

    todo

---
+++ src/main/java/com/group8/ClassroomPal/database/Database.java
@@ -0,0 +1,18 @@
+package com.group8.ClassroomPal.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Database {
+    private static final String URL = "jdbc:h2:./studentdb";
+
+    // The database interface.Gets this in your DAO
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(URL, "sa", "");
+    }
+
+    public static void init() {
+
+    }
+    }
--- src/main/java/com/group8/ClassroomPal/database/database.java
+++
@@ -1,7 +0,0 @@
-package com.group8.ClassroomPal.database;
-
-public class database {
-    // TODO:initialize the backend database
-    private void init() {}
-
-}
