551e8fdedfd73f73468639d991653623d1048ae2
timekeeper005 <99313318+timekeeper005@users.noreply.github.com>
2026-06-26 10:09:57 +0800

    todo

--- src/main/java/com/group8/ClassroomPal/database/Database.java
+++ src/main/java/com/group8/ClassroomPal/database/Database.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class Database {
-    private static final String URL = "jdbc:h2:./studentdb";
+    private static final String URL = "jdbc:h2:./keTangPai";
 
     // The database interface.Gets this in your DAO
     public static Connection getConnection() throws Exception {
